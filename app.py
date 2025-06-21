12dca87eaebe90bf25235b8045a1b3a85f74ca6b
dxninja-codes <dineshdx3548@gamil.com>
2025-06-22 00:43:38 +0530

    Created html file & added the basic playwright script

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from playwright.sync_api import sync_playwright
-def run(playwright):
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
-    page.goto("https://wwwgoogle.com")
 
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://www.google.com/")
+    
+    page.screenshot(path="google_screenshot.png")
+    
+    # Close the browser
     browser.close()
